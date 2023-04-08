e4fdc2f0615366e76b887a44c46d9361410a8f79
Arian24chander <126974755+Arian24chander@users.noreply.github.com>
2023-04-08 17:53:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').bxSlider();
+});
+$(document).ready(function () {
+    $('.slider_2').bxSlider();
+});
+$(document).ready(function () {
+    $('.slider_3').bxSlider();
+});
